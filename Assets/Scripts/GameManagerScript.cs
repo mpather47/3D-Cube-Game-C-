0c0eb9d6d1e0328df1cfe9e8f39b66543dda7211
Marcell <mpather47@gmail.com>
2020-06-21 11:28:17 +0200

    first commit

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    bool edge = false;
+    public void EndGame()
+    {
+        if (edge = false)
+        {
+            edge = true;
+            Debug.Log("Game Over");
+        }
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float fowardForce =100f;
+    public float sideWaysForce = 100f;
+    public Transform player;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+       
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, fowardForce*Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideWaysForce *Time.deltaTime,0,0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideWaysForce*Time.deltaTime,0,0, ForceMode.VelocityChange);
+        }
+
+        if (player.position.x <0)
+        {
+            FindObjectOfType<GameManagerScript>().EndGame();
+        }
+    }
+}
